8fc0145ee74cadae57c623055b5074a0f54fc92c
Amir Rezazadeh <amrezzd@gmail.com>
2022-02-19 22:20:40 +0330

    Add some comments

--- src/FlappyCube2d.hx
+++ src/FlappyCube2d.hx
@@ -2,53 +2,59 @@ import hxd.Timer;
 import h2d.Graphics;
 
 class FlappyCube2d extends h2d.Graphics {
-    public var mass : Float = 1;
-    public var gravity : Float = 9.807;
-    public var velocity = new h3d.Vector(0, 0);
-    public var rotationDirection = 0; // 0, 1 or -1
-    public var rotationSpeed = 2;
-    private var mouseGravity = -300;
-    
-    var lastGravityUpdatedTime : Float = 0;
-    var gravityUpdatePeriod : Float = 16.66 / 1000;
-    private var interaction : h2d.Interactive;
+	public var mass:Float = 1;
+	public var gravity:Float = 9.807;
+	public var velocity = new h3d.Vector(0, 0);
+	// 0 for no rotation, 1 for clockwise and -1 for couterclockwise rotation
+	public var rotationDirection = 0;
+	public var rotationSpeed = 2;
 
-    public function new(s2d : h2d.Scene) {
-        super(s2d);
-        interaction = new h2d.Interactive(s2d.width, s2d.height, s2d);
-        interaction.onPush = function(event : hxd.Event){
-            var xDistance = this.x - event.relX;
-            var yDixtance = this.y - event.relY;
-            var val = 1.0 / Math.sqrt((xDistance * xDistance) + (yDixtance * yDixtance));
-            xDistance *= val;
-            yDixtance *= val;
-            
-            velocity.y = -mouseGravity * yDixtance;
-            velocity.x = -mouseGravity * xDistance;
+	private var mouseGravity = -300;
 
-            rotationDirection = xDistance > 0 ? 1 : xDistance < 0 ? -1 : 0;
-        }
+	// apply gravity acceleration on 60 fps rate
+	private var lastGravityUpdatedTime:Float = 0;
+	private var gravityUpdatePeriod:Float = 16.66 / 1000;
 
-    }
+	private var interaction:h2d.Interactive;
+
+	public function new(s2d:h2d.Scene) {
+		super(s2d);
+		interaction = new h2d.Interactive(s2d.width, s2d.height, s2d);
+		interaction.onPush = function(event:hxd.Event) {
+			var xDistance = this.x - event.relX;
+			var yDixtance = this.y - event.relY;
+			// normalize the distance
+			var val = 1.0 / Math.sqrt((xDistance * xDistance) + (yDixtance * yDixtance));
+			xDistance *= val;
+			yDixtance *= val;
+
+			// apply mouse gravity
+			velocity.y = -mouseGravity * yDixtance;
+			velocity.x = -mouseGravity * xDistance;
+
+			rotationDirection = xDistance > 0 ? 1 : xDistance < 0 ? -1 : 0;
+		}
+	}
+
 	public function init(x, y, width, height, color) {
-        this.x = x;
-        this.y = y;
+		this.x = x;
+		this.y = y;
 
-        beginFill(color);
-        drawRect(0, 0, width, height);
-        endFill();
-    }
+		beginFill(color);
+		drawRect(0, 0, width, height);
+		endFill();
+	}
 
-    public function update(df: Float){
-        lastGravityUpdatedTime += df;
-        if ( lastGravityUpdatedTime > gravityUpdatePeriod) {
-            lastGravityUpdatedTime -= gravityUpdatePeriod;
-            velocity.y += gravity * mass;
-        }
+	public function update(df:Float) {
+		lastGravityUpdatedTime += df;
+		if (lastGravityUpdatedTime > gravityUpdatePeriod) {
+			lastGravityUpdatedTime -= gravityUpdatePeriod;
+			velocity.y += gravity * mass;
+		}
 
-        x += velocity.x * df;
-        y += velocity.y * df;
+		x += velocity.x * df;
+		y += velocity.y * df;
 
-        rotation += rotationDirection * rotationSpeed * df;
-    }
+		rotation += rotationDirection * rotationSpeed * df;
+	}
 }
--- src/FlappyCube2dGame.hx
+++ src/FlappyCube2dGame.hx
@@ -4,20 +4,19 @@ import h3d.scene.Graphics;
 import h3d.scene.Box;
 
 class FlappyCube2dGame extends hxd.App {
-	var flappyCube : FlappyCube2d;
-	
+	var flappyCube:FlappyCube2d;
+
 	override function init() {
 		flappyCube = new FlappyCube2d(s2d);
-		flappyCube.init(s2d.width / 2, 0, 20, 20, 0x0000FF);        
+		flappyCube.init(s2d.width / 2, 0, 20, 20, 0x0000FF);
 	}
 
 	override function update(df:Float) {
-        flappyCube.update(df);
-    }
+		flappyCube.update(df);
+	}
 
 	static function main() {
 		hxd.Res.initEmbed();
 		new FlappyCube2dGame();
 	}
-
 }
